bb6ea72827cef16a8f73b3472943cf74768854fc
Iona Macbeth <ionalmacbeth@gmail.com>
2018-06-25 15:21:36 +0100

    sheets method passing

--- src/test/java/TestPrinter.java
+++ src/test/java/TestPrinter.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestPrinter {
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer(50);
+
+        }
+
+        @Test
+         public void getSheets(){
+            assertEquals(50, printer.getSheets());
+    }
+
+
 }
